0086d864b9c1a9ec87698944cba92a7e163839f8
Dan Riding <doonyby@gmail.com>
2016-04-05 16:06:11 -0600

    modified prompt and appended loop

--- app.js
+++ app.js
@@ -1,22 +1,17 @@
 $(document).ready(function() {
 	var number = parseInt(prompt("Enter a Value", "0"), 10);
-
-
-			
-
-
-
-			/*for (var i = 1; i <= number; i++) {
-			if (i % 15 === 0) {
-				$("#output").append('<li>fizzbuzz</li>');
-			}
-			else if (i % 3 === 0) {
-				$("#output").append('<li>fizz</li>');
-			}
-			else if (i % 5 === 0) {
-				$("#output").append('<li>buzz</li>');
-			}
-			else {
-				$("#output").append('<li>' + i + '</li>');
-			}*/
+	for (var i = 1; i <= number; i++) {
+		if (i % 15 === 0) {
+			$("#output").append('<li>fizzbuzz</li>');
+		}
+		else if (i % 3 === 0) {
+			$("#output").append('<li>fizz</li>');
+		}
+		else if (i % 5 === 0) {
+			$("#output").append('<li>buzz</li>');
+		}
+		else {
+			$("#output").append('<li>' + i + '</li>');
+		}
+	};
 })
